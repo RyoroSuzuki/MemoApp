dde3972623d23d037432282633d10afb596b68a4
RyoroSuzuki <ryoro@kitchhike.com>
2021-12-09 21:29:42 +0900

    メモの編集、作成画面

--- App.jsx
+++ App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import MemoListScreen from './src/screen/MemoListScreen';
 import MemoDetailScreen from './src/screen/MemoDetailScreen';
+import MemoEditScreen from './src/screen/MemoEditScreen';
+import MemoCreateScreen from './src/screen/MemoCreateScreen';
 
 // eslint-disable-next-line react/function-component-definition
 export default function App() {
-  return <MemoListScreen />;
+  return <MemoCreateScreen />;
 }
